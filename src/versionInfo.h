e2a02010f1ea81c3a75228025f2aa70e88b04eb5
Tim Henning <tim@luminosuslight.de>
2018-07-24 09:19:49 +0200

    Increase version to 0.0.3.1.0.2

--- src/versionInfo.h
+++ src/versionInfo.h
@@ -25,7 +25,7 @@
 
 // This file includes the version number of this software.
 
-static const QString VERSION_STRING = "0.0.3.1.0.1";
+static const QString VERSION_STRING = "0.0.3.1.0.2";
 static const int SETTINGS_FORMAT_VERSION = 1;
 
 #endif // VERSIONINFO_H
